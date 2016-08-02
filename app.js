d309dda4cdf164b8abcfa44a1ddf1842d6fed50b
serashioda <seras37@gmail.com>
2016-08-01 23:44:49 -0700

    images displayed, added event listener/handler; display images; counter

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-var catalogArray = []
+'use strict';
 
-function catalogItem(imageName, filePath) {
+// GLOBAL DECLARATION
+var imageArray = [];
+var shownItems = [];
+
+// OBJECT
+function CatalogItem(imageName, filePath) {
   this.imageName = imageName;
   this.filePath = filePath;
   this.tallyclicked = 0;
@@ -9,6 +14,75 @@ function catalogItem(imageName, filePath) {
   imageArray.push(this);
 }
 
-new catalogItem( , )
+new CatalogItem('Bag', 'images/bag.jpg'); //eslint-disable-line
+new CatalogItem('Banana', 'images/banana.jpg');
+new CatalogItem('Bathroom', 'images/bathroom.jpg');
+new CatalogItem('Boots', 'images/boots.jpg');
+new CatalogItem('Breakfast', 'images/breakfast.jpg');
+new CatalogItem('Bubblegum', 'images/bubblegum.jpg');
+new CatalogItem('Chair', 'images/chair.jpg');
+new CatalogItem('Cthulhu', 'images/cthulhu.jpg');
+new CatalogItem('DuckMuzzle', 'images/dog-duck.jpg');
+new CatalogItem('Dragon', 'images/dragon.jpg');
+new CatalogItem('Pen', 'images/pen.jpg');
+new CatalogItem('PetSlippers', 'images/pet-sweep.jpg');
+new CatalogItem('Scissors', 'images/scissors.jpg');
+new CatalogItem('Shark', 'images/shark.jpg');
+new CatalogItem('Sweep', 'images/sweep.jpg');
+new CatalogItem('Tauntaun', 'images/tauntaun.jpg');
+new CatalogItem('Unicorn', 'images/unicorn.jpg');
+new CatalogItem('Usb', 'images/usb.jpg');
+new CatalogItem('WateringCan', 'images/water-can.jpg');
+new CatalogItem('WineGlass', 'images/wine-glass.jpg');
 
-var = randomValue = catalogArray[math.floor(math.random()*catalogArray.length)];
+
+// function ___ (newCatalogItem){
+function getNewRandomImages() {
+var randomValue = getRandomImage();
+var leftImg = document.getElementById('left');
+leftImg.src = randomValue.filePath;
+leftImg.alt = randomValue.imageName;
+leftImg.addEventListener('click', imageClicked);
+
+randomValue = getRandomImage();
+var centerImg = document.getElementById('center');
+centerImg.src = randomValue.filePath;
+centerImg.alt = randomValue.imageName;
+centerImg.addEventListener('click', imageClicked);
+
+randomValue = getRandomImage();
+var rightImg = document.getElementById('right');
+rightImg.src = randomValue.filePath;
+rightImg.alt = randomValue.imageName;
+rightImg.addEventListener('click', imageClicked);
+}
+
+//GET RANDOM ELEMENT & ENSURE NOT DUPE
+function getRandomImage () {
+  while (true){
+    var randomValue = imageArray[Math.floor(Math.random() * imageArray.length)];
+    if (shownItems.indexOf(randomValue.imageName) === -1){
+      //IMAGE HAS NOT BEEN SHOWN
+      shownItems.push(randomValue.imageName);
+      if (shownItems.length > 6) {
+        shownItems.pop();
+      }
+      randomValue.tallyDisplayed++;
+      console.log(randomValue.tallyDisplayed);
+      return randomValue;
+
+    }
+  }
+}
+
+function imageClicked(element) {
+  console.log(element.target.alt);
+
+  for (var i = 0; i < imageArray.length; i++) {
+    if (imageArray[i].imageName === element.target.alt) {
+      imageArray[i].tallyclicked++;
+    }
+  }
+  getNewRandomImages();
+}
+getNewRandomImages();
